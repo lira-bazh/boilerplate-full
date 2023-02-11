be8933ff3fd14382380383932c81e64c9cc4dd38
Irina Bazh <79859948912@ya.ru>
2023-02-11 20:41:29 +0300

    обновление пакетов

--- client/main.jsx
+++ client/main.jsx
@@ -1,13 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from "react-dom/client";
 import Root from "./config/root"
 
 import './assets/styles/style.scss'
 
 const target = document.getElementById('root')
 
-const render = (Component) => {
-  ReactDOM.render(<Component />, target)
-}
-
-render(Root)
+const root = createRoot(target);
+root.render(Root());
